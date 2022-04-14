b3c6880f50ec7f0c326531e7581129cd5444085e
Stanislav Efremov <efremov.stanislav.v@gmail.com>
2022-04-14 11:08:44 +0300

    Initial commit
    Source code and cmake files for app and lib
    
    Signed-off-by: Stanislav Efremov <efremov.stanislav.v@gmail.com>

---
+++ lib.cpp
@@ -0,0 +1,7 @@
+#include "lib.h"
+#include "version.h"
+
+unsigned int get_version()
+{
+    return APP_VERSION;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "lib.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Version " <<  get_version() << std::endl;
+    return 0;
+}
